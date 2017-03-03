9f2b3001ca367d0ff019f9d96fccc1a3a2ff9339
Greg Walker <michael.walker@gsa.gov>
2017-03-02 20:28:42 -0600

    Hide header links if not logged in

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,7 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
-export default function header(props) {
+function getNavigationLinks(user) {
+  if (user) {
+    return (
+      <nav role="navigation" className="usa-nav">
+        <button className="usa-nav-close">
+          <img src="/assets/img/close.svg" alt="close" />
+        </button>
+        <ul className="usa-nav-primary usa-accordion">
+          <li>
+            <Link className="usa-nav-link" to="/eligibility">
+              <span>Eligibility</span>
+            </Link>
+          </li>
+          <li>
+            <Link className="usa-nav-link" to="/coverage">
+              <span>Coverage</span>
+            </Link>
+          </li>
+          {
+          // <li>
+          //   <button className="
+          //   usa-accordion-button usa-nav-link" aria-expanded="false" aria-controls="side-nav-1">
+          //     <span>Coverage</span>
+          //   </button>
+          //   <ul id="side-nav-1" className="usa-nav-submenu">
+          //     <li>
+          //       <a href="#">Subsection title</a>
+          //     </li>
+          //     <li>
+          //       <a href="#">Subsection title</a>
+          //     </li>
+          //     <li>
+          //       <a href="#">Subsection title</a>
+          //     </li>
+          //   </ul>
+          // </li>
+          }
+          <li>
+            <Link className="usa-nav-link" to="/payments">
+              <span>Payments</span>
+            </Link>
+          </li>
+        </ul>
+        {
+        // <form className="usa-search usa-search-small">
+        //   <div role="search">
+        //     <label className="usa-sr-only" htmlFor="search-field-small">Search small</label>
+        //     <input id="search-field-small" type="search" name="search" />
+        //     <button type="submit">
+        //       <span className="usa-sr-only">Search</span>
+        //     </button>
+        //   </div>
+        // </form>
+        }
+      </nav>
+    );
+  }
+  return null;
+}
+
+function header(props) {
   return (
     <header className="usa-header usa-header-basic main" role="banner">
       <div className="usa-banner">
@@ -22,59 +83,16 @@ export default function header(props) {
             </em>
           </div>
         </div>
-        <nav role="navigation" className="usa-nav">
-          <button className="usa-nav-close">
-            <img src="/assets/img/close.svg" alt="close" />
-          </button>
-          <ul className="usa-nav-primary usa-accordion">
-            <li>
-              <Link className="usa-nav-link" to="/eligibility">
-                <span>Eligibility</span>
-              </Link>
-            </li>
-            <li>
-              <Link className="usa-nav-link" to="/coverage">
-                <span>Coverage</span>
-              </Link>
-            </li>
-            {
-            // <li>
-            //   <button className="
-            //   usa-accordion-button usa-nav-link" aria-expanded="false" aria-controls="side-nav-1">
-            //     <span>Coverage</span>
-            //   </button>
-            //   <ul id="side-nav-1" className="usa-nav-submenu">
-            //     <li>
-            //       <a href="#">Subsection title</a>
-            //     </li>
-            //     <li>
-            //       <a href="#">Subsection title</a>
-            //     </li>
-            //     <li>
-            //       <a href="#">Subsection title</a>
-            //     </li>
-            //   </ul>
-            // </li>
-            }
-            <li>
-              <Link className="usa-nav-link" to="/payments">
-                <span>Payments</span>
-              </Link>
-            </li>
-          </ul>
-          {
-          // <form className="usa-search usa-search-small">
-          //   <div role="search">
-          //     <label className="usa-sr-only" htmlFor="search-field-small">Search small</label>
-          //     <input id="search-field-small" type="search" name="search" />
-          //     <button type="submit">
-          //       <span className="usa-sr-only">Search</span>
-          //     </button>
-          //   </div>
-          // </form>
-          }
-        </nav>
+        {getNavigationLinks(props.user)}
       </div>
     </header>
   );
 };
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps)(header);
