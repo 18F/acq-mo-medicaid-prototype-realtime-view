0c26a21973c5ab7f0c4341d7207753fb98ec734a
Greg Walker <michael.walker@gsa.gov>
2017-03-06 11:32:15 -0600

    Always set the CORS headers.

--- api/index.js
+++ api/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-function cors(req, res) {
+app.use((req, res, next) => {
+  // Note that the proper thing to do here is actually
+  // respond on a per-endpoing basis.  I.e., once we know
+  // what endpoints are available in the API, this should
+  // respond with just the set of verbs that the endpoint
+  // supports, or it should fail if the endpoint does NOT
+  // exist.
   res.header('Access-Control-Allow-Origin', req.get('origin'));
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length');
-}
 
-app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
-    cors(req, res);
+    // If it's an OPTIONS request, just bail out here.
     res.send(200);
   } else {
     next();
@@ -20,7 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/login', (req, res) => {
-  cors(req, res);
   res.send({
     username: req.body.username,
     realName: 'Jane Doe',
