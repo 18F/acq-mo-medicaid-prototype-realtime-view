56026c196b0ae779590f41d8628e6451d6548c20
Greg Walker <michael.walker@gsa.gov>
2017-03-06 17:58:04 -0600

    Sketch out some structure for the eligibility display - current user vs. some other user

--- web/src/components/eligibility/index.js
+++ web/src/components/eligibility/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Eligibility as actions } from '../../actions';
 
-export default function eligibility() {
-  return (
-    <div>Check if you’re eligible!</div>
-  );
-};
+class Eligibility extends React.Component {
+  render() {
+    let target = this.props.user;
+    if(this.props.params && this.props.params.participantID) {
+      target = this.props.participants.find(participant => participant.id === this.props.params.participantID);
+      return (
+        <div>{target.realName} is {target.eligible ? '' : 'not'} eligible.</div>
+      );
+    } else if (this.props.user) {
+      return (
+        <div>You&lsquo;re {target.eligible ? '' : 'not currently'} eligible for Medicaid services</div>
+      );
+    }
+
+    return null;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+    participants: state.participants
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getEligibilityForParticipant(participantID) {
+      dispatch(actions.getEligibilityForParticipant(participantID));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Eligibility);
