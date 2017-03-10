94a1beaae8ed3fcad3a78dcca36381f2c050c858
Greg Walker <michael.walker@gsa.gov>
2017-03-10 10:12:31 -0600

    Add credentials for deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,24 +13,23 @@ SPACE="michael.walker"
 API_NAME="missouri-medicaid-prototype-api"
 FRONTEND_NAME="missouri-medicaid-prototype-frontend"
 
-# # This directory is used to persist the CF credentials
-# mkdir -p $HOME/.cf
-#
-# This wonderful image pulls the `cf` tool along with the
-# `autopilot` plugin
-# docker pull adelevie/cf-cli:latest
-#
-# For some reason, aliases aren't working here
-# so we're using this function instead
-# cf_run() {
-#  docker run \
-#    --rm \
-#    -v $HOME/.cf:/root/.cf \
-#    -v $PWD:/app \
-#    adelevie/cf-cli \
-#    cf "$@"
-# }
-#
+# This directory is used to persist the CF credentials
+mkdir -p $HOME/.cf
+
+This wonderful image pulls the `cf` tool along with the
+`autopilot` plugin
+docker pull adelevie/cf-cli:latest
+
+For some reason, aliases aren't working here
+so we're using this function instead
+cf_run() {
+ docker run \
+   --rm \
+   -v $HOME/.cf:/root/.cf \
+   -v $PWD:/app \
+   adelevie/cf-cli \
+   cf "$@"
+}
 
 # Build the front-end
 cd web
@@ -39,6 +38,6 @@ cp src/env.prod.json src/env.json
 npm run build
 cd ..
 
-# # Log into CF and push
-# cf_run login -a $API -u $CF_USERNAME -p $CF_PASSWORD -o $ORG -s $SPACE
-# cf_run zero-downtime-push -f manifest.yml
+# Log into CF and push
+cf_run login -a $API -u $CF_USERNAME -p $CF_PASSWORD -o $ORG -s $SPACE
+cf_run zero-downtime-push -f manifest.yml
